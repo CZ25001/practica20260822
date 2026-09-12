d38cbf8a6ea529de25fa009ad7fa766b9e3fd98d
ZC25001 <zc25001@esfe.agape.edu.sv>
2026-09-12 15:16:12 -0600

    app.js actualizado

--- src/app.js
+++ src/app.js
@@ -10,9 +10,10 @@ const app = express();
 
 // Configuración de CORS
 const corsOptions = {
-  origin: process.env.CLIENT_URL || '*', // O especifica tu frontend (ej: 'http://localhost:5173')
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+    origin: process.env.CLIENT_URL || "*", // O especifica tu frontend (ej: 'http://localhost:5173')
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-use-cookie']
 };
 
 app.use(cors(corsOptions)); // Aplicar CORS globalmente
